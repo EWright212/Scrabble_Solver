0ba41d9a2b64d6113029844e99d21b1645fd4906
Megan Hayes <megan.hayes@hotmail.co.uk>
2020-06-18 10:42:52 +0100

    method functioning properly for a and f

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -5,13 +5,16 @@ class Scrabble
   end
 
   def score
-    if @word ==  'a'
-      1
-    elsif @word == 'aa'
-      2
-    else
-      4
+    total = 0
+    split_word = @word.split(//)
+    split_word.each do |letter|
+      if letter ==  'a'
+        total += 1
+      elsif letter == 'f'
+        total += 4
+      end
     end
+    return total
   end
 
 end
